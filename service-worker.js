73930a1f9183142b30113b9ac1d4d499d930265d
skytestsiteform-oss <skytestsiteform@gmail.com>
2026-03-22 00:51:41 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,11 @@
+// Always fetch from network, never serve stale cache
+self.addEventListener('install', e => { self.skipWaiting(); });
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
